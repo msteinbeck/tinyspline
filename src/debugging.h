51b90f3f0fb2fa6c7e511e95f8aef8bae04cfe4b
Marcel <marcel@tzi.de>
2014-12-18 15:16:12 +0100

    added header with code only for debugging not for release

---
+++ src/debugging.h
@@ -0,0 +1,21 @@
+#include "tinyspline.h"
+
+void ts_bspline_print(tsBSpline* bspline)
+{
+    int i, j;
+    
+    printf("########### control points (%zu) ###########\n", bspline->n_ctrlp);
+    for (i = 0; i < bspline->n_ctrlp; i++) {
+        for (j = 0; j < bspline->dim; j++) {
+            printf("%f\t", bspline->ctrlp[i * bspline->dim + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+    printf("########### knots(%zu) ###########\n", bspline->n_knots);
+    for (i = 0; i < bspline->n_knots; i++) {
+        printf("%f  ", bspline->knots[i]);
+    }
+    printf("\n\n");
+}

e3805c861b24bb9ea7b556685175475f467bba95
marcel <marcel@tzi.de>
2015-03-24 10:37:52 +0100

    added class TsBSpline

--- src/tinysplinecpp.cpp
+++ src/tinysplinecpp.cpp
@@ -7,11 +7,16 @@ TsBSpline::TsBSpline()
 
 TsBSpline::TsBSpline(TsBSpline& other)
 {
-    ts_bspline_copy(&other.bspline, &bspline);
+    const tsError err = ts_bspline_copy(&other.bspline, &bspline);
+    if (err < 0)
+        throw err;
 }
 
-TsBSpline::TsBSpline(const size_t deg, const size_t dim, const size_t n_ctrlp, const tsBSplineType type) {
-    ts_bspline_new(deg, dim, n_ctrlp, type, &bspline);
+TsBSpline::TsBSpline(const size_t deg, const size_t dim, const size_t n_ctrlp, 
+                     const tsBSplineType type) {
+    const tsError err = ts_bspline_new(deg, dim, n_ctrlp, type, &bspline);
+    if (err < 0)
+        throw err;
 }
 
 TsBSpline::~TsBSpline()
@@ -24,37 +29,123 @@ tsBSpline* TsBSpline::data()
     return &bspline;
 }
 
-size_t TsBSpline::deg()
-{
-    return bspline.deg;
-}
-
-size_t TsBSpline::order()
-{
-    return bspline.order;
-}
-
-size_t TsBSpline::dim()
-{
-    return bspline.dim;
-}
-
-size_t TsBSpline::nCtrlp() 
-{
-    return bspline.n_ctrlp;
-}
-
-size_t TsBSpline::nKnots()
-{
-    return bspline.n_knots;
-}
-
-float* TsBSpline::ctrlp()
+float* TsBSpline::ctrlp() const
 {
     return bspline.ctrlp;
 }
 
-float* TsBSpline::konts()
+float* TsBSpline::knots() const
 {
     return bspline.knots;
+}
+
+size_t TsBSpline::deg() const
+{
+    return bspline.deg;
+}
+
+size_t TsBSpline::order() const
+{
+    return bspline.order;
+}
+
+size_t TsBSpline::dim() const
+{
+    return bspline.dim;
+}
+
+size_t TsBSpline::nCtrlp() const
+{
+    return bspline.n_ctrlp;
+}
+
+size_t TsBSpline::nKnots() const
+{
+    return bspline.n_knots;
+}
+
+void TsBSpline::setDeg(const size_t deg)
+{
+    if (deg >= bspline.n_ctrlp)
+        throw TS_DEG_GE_NCTRLP;
+    const size_t order = deg+1;
+    if (order < deg)
+        throw TS_OVER_UNDERFLOW;
+    bspline.deg = deg;
+    bspline.order = order;
+}
+
+void TsBSpline::setOrder(const size_t order)
+{
+    const size_t deg = order-1;
+    if (deg > order)
+        throw TS_OVER_UNDERFLOW;
+    if (deg >= bspline.n_ctrlp)
+        throw TS_DEG_GE_NCTRLP;
+    bspline.order = order;
+    bspline.deg = deg;
+}
+
+void TsBSpline::setDim(const size_t dim)
+{
+    if (dim == 0)
+        throw TS_DIM_ZERO;
+    bspline.dim = dim;
+}
+
+void TsBSpline::setNCtrlp(const size_t nCtrlp)
+{
+    if (bspline.deg >= nCtrlp)
+        throw TS_DEG_GE_NCTRLP;
+    bspline.n_ctrlp = nCtrlp;
+}
+
+void TsBSpline::setNKnots(const size_t nKnots)
+{
+    // TODO: add error code to c lib
+    bspline.n_knots = nKnots;
+}
+
+void TsBSpline::setupKnots(const tsBSplineType type)
+{
+    const tsError err = ts_bspline_setup_knots(&bspline, type, &bspline);
+    if (err < 0)
+        throw err;
+}
+
+void TsBSpline::insertKnot(const float u, const size_t n)
+{
+    const tsError err = ts_bspline_insert_knot(&bspline, u, n, &bspline);
+    if (err < 0)
+        throw err;
+}
+
+void TsBSpline::resize(const int n, const int back)
+{
+    const tsError err = ts_bspline_resize(&bspline, n, back, &bspline);
+    if (err < 0)
+        throw err;
+}
+
+size_t TsBSpline::split(const float u)
+{
+    size_t k;
+    const tsError err = ts_bspline_split(&bspline, u, &bspline, &k);
+    if (err < 0)
+        throw err;
+    return k;
+}
+
+void TsBSpline::buckle(const float b)
+{
+    const tsError err = ts_bspline_buckle(&bspline, b, &bspline);
+    if (err < 0)
+        throw err;
+}
+
+void TsBSpline::toBesziers()
+{
+    const tsError err = ts_bspline_to_beziers(&bspline, &bspline);
+    if (err < 0)
+        throw err;
 }
--- src/tinysplinecpp.h
+++ src/tinysplinecpp.h
@@ -6,18 +6,33 @@ class TsBSpline {
 public:
     TsBSpline();
     TsBSpline(TsBSpline& other);
-    TsBSpline(const size_t deg, const size_t dim, const size_t n_ctrlp, const tsBSplineType type);
+    TsBSpline(const size_t deg, const size_t dim, const size_t n_ctrlp, 
+              const tsBSplineType type);
     ~TsBSpline();
     
     tsBSpline* data();
+    float* ctrlp() const;
+    float* knots() const;
     
-    size_t deg();
-    size_t order();
-    size_t dim();
-    size_t nCtrlp();
-    size_t nKnots();
-    float* ctrlp();
-    float* konts();
+    // getter
+    size_t deg() const;
+    size_t order() const;
+    size_t dim() const;
+    size_t nCtrlp() const;
+    size_t nKnots() const;
+    // setter
+    void setDeg(const size_t deg);
+    void setOrder(const size_t order);
+    void setDim(const size_t dim);
+    void setNCtrlp(const size_t nCtrlp);
+    void setNKnots(const size_t nKnots);
+    
+    void setupKnots(const tsBSplineType type);
+    void insertKnot(const float u, const size_t n);
+    void resize(const int n, const int back);
+    size_t split(const float u);
+    void buckle(const float b);
+    void toBesziers();
     
 private:
     tsBSpline bspline;

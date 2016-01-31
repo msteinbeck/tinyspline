c5ce2a9d639e5bc55d93113964b118730f34ed4d
egradman <eric@gradman.com>
2016-01-30 18:49:47 -0800

    support command-line compilation on OSX

--- example/interpolation.c
+++ example/interpolation.c
@@ -1,8 +1,15 @@
 #include "tinyspline.h"
 
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#endif
+
 #include <stdio.h>
 #include <debugging.h>
 

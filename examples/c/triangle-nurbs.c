9c53e2da380414a48c2bafae17f884c4db97e29d
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2016-06-19 00:08:54 +0200

    Added an example for triangle using NURBS instead of knot insertion. Fixes #56.

---
+++ examples/c/triangle-nurbs.c
@@ -0,0 +1,162 @@
+#include "tinyspline.h"
+
+#if defined(__APPLE__)
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h> 
+#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <tinyspline.h>
+
+tsBSpline spline;
+GLUnurbsObj *theNurb;
+// the original second point of `spline` that is
+// weight with `w` in every call of ::display
+float B[4] = {1.0f, 1.0f, 0.0f, 1.0f};
+float w = 0.001f;
+
+/********************************************************
+*                                                       *
+* Modify these lines for experimenting.                 *
+*                                                       *
+********************************************************/
+void setup()
+{
+    ts_bspline_new(
+        2,      // degree of spline
+        4,      // dimension of each point
+        3,      // number of control points
+        TS_CLAMPED,// used to hit first and last control point
+        &spline // the spline to setup
+    );
+    
+    // Setup control points.
+    spline.ctrlp[0] = -1.0f;
+    spline.ctrlp[1] = 1.0f;
+    spline.ctrlp[2] = 0.0f;
+    spline.ctrlp[3] = 1.0f;
+
+    // ignore second control point here since it is
+    // updated in ::display anyway
+
+    spline.ctrlp[8] = 1.0f;
+    spline.ctrlp[9] = -1.0f;
+    spline.ctrlp[10] = 0.0f;
+    spline.ctrlp[11] = 1.0f;
+}
+
+void tear_down()
+{
+    ts_bspline_free(&spline);
+}
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // weight the first three components with `w` and store `w`
+    // in the forth component of the control point---that's how
+    // homogeneous coordinates work
+    spline.ctrlp[4] = B[0] * w;
+    spline.ctrlp[5] = B[1] * w;
+    spline.ctrlp[6] = B[2] * w;
+    spline.ctrlp[7] = w;
+    
+    // draw spline
+    glColor3f(1.0, 1.0, 1.0);
+    glLineWidth(3);
+    gluBeginCurve(theNurb);
+        gluNurbsCurve(
+            theNurb, 
+            (GLint)spline.n_knots,
+            spline.knots,
+            (GLint)spline.dim,
+            spline.ctrlp,
+            (GLint)spline.order,
+            GL_MAP1_VERTEX_4
+        );
+    gluEndCurve(theNurb);
+
+    // draw control points
+    glColor3f(1.0, 0.0, 0.0);
+    glPointSize(5.0);
+    size_t i;
+    glBegin(GL_POINTS);
+         glVertex3fv(spline.ctrlp);
+         glVertex3fv(B);
+         glVertex3fv(spline.ctrlp + 8);
+    glEnd();
+    
+    glutSwapBuffers();
+    glutPostRedisplay();
+
+    w *= 1.05; // lon-linear update of `w`
+    if (w > 100.f) {
+        w = 0.001f;
+    }
+}
+
+
+
+
+/********************************************************
+*                                                       *
+* Framework                                             *
+*                                                       *
+********************************************************/
+void nurbsError(GLenum errorCode)
+{
+   const GLubyte *estring;
+
+   estring = gluErrorString(errorCode);
+   fprintf (stderr, "Nurbs Error: %s\n", estring);
+   exit (0);
+}
+   
+void init(void)
+{
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    theNurb = gluNewNurbsRenderer();
+    gluNurbsProperty (theNurb, GLU_SAMPLING_TOLERANCE, 10.0);
+    gluNurbsCallback(theNurb, GLU_ERROR, (GLvoid (*)()) nurbsError);
+    setup();
+}
+
+void reshape(int w, int h)
+{
+   glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   gluPerspective (45.0, (GLdouble)w/(GLdouble)h, 3.0, 8.0);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   glTranslatef (0.0, 0.0, -5.0);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow(argv[0]);
+    init();
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    tear_down();
+    return 0; 
+}

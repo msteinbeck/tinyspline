ea7d94197bb30e546dcbbdc550ca1e85d0709159
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2016-12-07 09:42:51 +0100

    Added tests for str_enum.

--- tests/c/ctests.c
+++ tests/c/ctests.c
@@ -5,6 +5,7 @@ CuSuite* get_default_suite();
 CuSuite* get_free_suite();
 CuSuite* get_distance_suite();
 CuSuite* get_arr_fill_suite();
+CuSuite* get_str_enum_suite();
 
 int main()
 {
@@ -15,6 +16,7 @@ int main()
     CuSuiteAddSuite(suite, get_free_suite());
     CuSuiteAddSuite(suite, get_distance_suite());
     CuSuiteAddSuite(suite, get_arr_fill_suite());
+    CuSuiteAddSuite(suite, get_str_enum_suite());
 
     CuSuiteRun(suite);
     CuSuiteSummary(suite, output);

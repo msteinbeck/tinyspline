e376a85d5f1dd6b82e1322954553c237ab713b12
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2018-01-17 16:20:50 +0100

    Make ci scripts more robust.

--- ci/compile.sh
+++ ci/compile.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 set -e
 
-# Directory where the script is located.
-SCRIPT_DIR=$( cd $(dirname $0); pwd -P)
-
 # Check required variables.
 if [ -z "$BUILD_DIR" ]; then
-	echo "'BUILD_DIR' has not been set or is empty; aborting."
+	echo "BUILD_DIR has not been set or is empty; aborting."
 	exit -1
 fi
 
+# Directory where this script is located.
+SCRIPT_DIR=$( cd $(dirname $0); pwd -P)
+
+# Make BUILD_DIR next to this script.
+BUILD_DIR_FIXED="$SCRIPT_DIR/$BUILD_DIR"
+
 # Build all targets.
-mkdir -p $BUILD_DIR
-pushd $BUILD_DIR
+mkdir -p "$BUILD_DIR_FIXED"
+pushd "$BUILD_DIR_FIXED"
 	cmake "$SCRIPT_DIR/.." $CMAKE_FLAGS
 	cmake --build .
 popd
--- ci/deploy_docs.sh
+++ ci/deploy_docs.sh
@@ -5,8 +5,9 @@ set -e
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-# Check environment to prevent multiple or unwanted deployments.
-if [ "$DEPLOY_DOCS" != "yes" ]                     \
+# Prevent multiple or unwanted deployments.
+if [ "$TRAVIS" != "true" ]                         \
+	|| [ "$DEPLOY_DOCS" != "true" ]            \
 	|| [ "$TRAVIS_OS_NAME" != "linux" ]        \
 	|| [ "$CC" != "clang" ]                    \
 	|| [ "$TRAVIS_PULL_REQUEST" != "false" ]   \
@@ -14,40 +15,63 @@ if [ "$DEPLOY_DOCS" != "yes" ]                     \
 	exit 0
 fi
 
-# Check required variables.
-if [ -z "$BUILD_DIR" ] || [ ! -d "$BUILD_DIR" ]; then
-	echo "Build directory is not available; aborting."
-	exit -1
-fi
-
-# Locations of docs to deploy.
-DOXYGEN_HTML_DIR="$BUILD_DIR/doxygen/html"
-
-# Make sure docs have been created.
-if [ ! -d "$DOXYGEN_HTML_DIR" ]; then
-	echo "HTML output ($DOXYGEN_HTML_DIR) is not available; aborting."
-	exit -1
-fi
-
 # Store some useful information.
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
+# Directory where this script is located.
+SCRIPT_DIR=$( cd $(dirname $0); pwd -P)
+
+# Verify that this script is next to 'compile.sh'.
+if [ ! -f "$SCRIPT_DIR/compile.sh" ]; then
+    echo "Unable to find 'compile.sh'; aborting."
+    exit -1
+fi
+# See 'compile.sh'.
+BUILD_DIR_FIXED="$SCRIPT_DIR/$BUILD_DIR"
+
+# Path to the docs that will be deployed.
+DOXYGEN_HTML_DIR="$BUILD_DIR_FIXED/doxygen/html"
+# Verify that docs have been created.
+if [ ! -d "$DOXYGEN_HTML_DIR" ]; then
+	echo "Doxygen HTML output is not available; aborting."
+	exit -1
+fi
+
+# Path to unencrypted deploy key.
+DEPLOY_KEY_PATH="$SCRIPT_DIR/deploy_key"
+# Path to encrypted deploy key.
+ENC_DEPLOY_KEY_PATH="$DEPLOY_KEY_PATH.enc"
+# Verify that encrypted deploy is available.
+if [ ! -f "$ENC_DEPLOY_KEY_PATH" ]; then
+    echo "Unable to find encrypted deploy key; aborting."
+    exit -1
+fi
 # Decrypt and install deploy key.
 openssl aes-256-cbc \
 	-K $encrypted_6b814194a991_key \
 	-iv $encrypted_6b814194a991_iv \
-	-in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
+	-in "$ENC_DEPLOY_KEY_PATH"     \
+	-out "$DEPLOY_KEY_PATH"        \
+	-d
+chmod 600 "$DEPLOY_KEY_PATH"
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add "$DEPLOY_KEY_PATH"
 
-# Clone the docs branch to 'TARGET_BRANCH'.
-git clone -b $TARGET_BRANCH $SSH_REPO $TARGET_BRANCH
-pushd $TARGET_BRANCH
+# Directory where TARGET_BRANCH is cloned to.
+TARGET_DIR="$SCRIPT_DIR/$TARGET_BRANCH"
+# Verify that TARGET_DIR does not exist.
+if [ -d "$TARGET_DIR" ]        \
+	|| [ -f "$TARGET_DIR" ]; then
+	echo "'$TARGET_DIR' already exists; aborting."
+	exit -1
+fi
+# Clone TARGET_BRANCH to TARGET_DIR.
+git clone -b $TARGET_BRANCH $SSH_REPO $TARGET_DIR
+pushd "$TARGET_DIR"
 	# Copy docs.
-	cp -a "../$DOXYGEN_HTML_DIR/." ./
+	cp -a "$DOXYGEN_HTML_DIR/." ./
 
 	# Set user name and email for commit.
 	git config user.name "Travis CI"
@@ -62,4 +86,4 @@ pushd $TARGET_BRANCH
 popd
 
 # Remove decrypted deploy key.
-rm deploy_key
+rm "$DEPLOY_KEY_PATH"

65e838a8b32c3cde5150013c2891af717c9ec37e
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2018-07-13 17:26:54 +0200

    Fixed C tests.

--- test/c/free.c
+++ test/c/free.c
@@ -1,55 +1,32 @@
 #include "tinyspline.h"
 #include "CuTest.h"
 #include <stdlib.h>
+#include <tinyspline.h>
 
 void free_test_bspline(CuTest* tc)
 {
-	tsBSpline b;
-	b.deg = 3;
-	b.order = 4;
-	b.dim = 2;
-	b.n_ctrlp = 10;
-	b.n_knots = 14;
-	b.ctrlp = (tsReal*) malloc(10*2*4);
-	b.knots = b.ctrlp + 5;
+	tsBSpline spline;
 
-	CuAssertPtrNotNull(tc, b.ctrlp);
-	CuAssertPtrNotNull(tc, b.knots);
+	ts_bspline_new(10, 2, 2, TS_CLAMPED, &spline);
+	CuAssertPtrNotNull(tc, spline.pImpl);
 
-	ts_bspline_free(&b);
-	CuAssertTrue(tc, b.deg == 0);
-	CuAssertTrue(tc, b.order == 0);
-	CuAssertTrue(tc, b.dim == 0);
-	CuAssertTrue(tc, b.n_ctrlp == 0);
-	CuAssertTrue(tc, b.n_knots == 0);
-	CuAssertPtrEquals(tc, b.ctrlp, NULL);
-	CuAssertPtrEquals(tc, b.knots, NULL);
+	ts_bspline_free(&spline);
+	CuAssertPtrEquals(tc, spline.pImpl, NULL);
 }
 
 void free_test_deboornet(CuTest* tc)
 {
-	tsDeBoorNet net;
-	net.u = 3.14f;
-	net.k = 4;
-	net.s = 2;
-	net.h = 5;
-	net.dim = 15;
-	net.n_points = 156;
-	net.points = (tsReal*) malloc(156*15*4);
-	net.result = net.points + 50;
+	tsBSpline spline;
+	ts_bspline_new(10, 2, 2, TS_CLAMPED, &spline);
 
-	CuAssertPtrNotNull(tc, net.result);
-	CuAssertPtrNotNull(tc, net.points);
+	tsDeBoorNet net;
+	ts_bspline_eval(&spline, 0, &net);
+	CuAssertPtrNotNull(tc, net.pImpl);
 
 	ts_deboornet_free(&net);
-	CuAssertTrue(tc, net.u == 0.f);
-	CuAssertTrue(tc, net.k == 0);
-	CuAssertTrue(tc, net.s == 0);
-	CuAssertTrue(tc, net.h == 0);
-	CuAssertTrue(tc, net.dim == 0);
-	CuAssertTrue(tc, net.n_points == 0);
-	CuAssertPtrEquals(tc, net.points, NULL);
-	CuAssertPtrEquals(tc, net.result, NULL);
+	CuAssertPtrEquals(tc, net.pImpl, NULL);
+
+	ts_bspline_free(&spline);
 }
 
 CuSuite* get_free_suite()
--- test/c/tests.c
+++ test/c/tests.c
@@ -4,7 +4,6 @@
 CuSuite* get_distance_suite();
 CuSuite* get_arr_fill_suite();
 CuSuite* get_str_enum_suite();
-CuSuite* get_default_suite();
 CuSuite* get_free_suite();
 CuSuite* get_new_suite();
 CuSuite* get_move_suite();
@@ -17,7 +16,6 @@ int main()
 	CuSuiteAddSuite(suite, get_distance_suite());
 	CuSuiteAddSuite(suite, get_arr_fill_suite());
 	CuSuiteAddSuite(suite, get_str_enum_suite());
-	CuSuiteAddSuite(suite, get_default_suite());
 	CuSuiteAddSuite(suite, get_free_suite());
 	CuSuiteAddSuite(suite, get_new_suite());
 	CuSuiteAddSuite(suite, get_move_suite());

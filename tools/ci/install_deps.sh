fc1ef9c759edf734ec3e9dec0e8f2bdb0ab26f84
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2018-07-30 23:59:24 +0200

    Run tar with sudo to copy files to /usr/local.

--- tools/ci/install_deps.sh
+++ tools/ci/install_deps.sh
@@ -35,7 +35,7 @@ pushd "$DEPS_DIR"
 
 	# Install CMake-3.12.0 from binary package.
 	wget https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.tar.gz
-	tar xf cmake-3.12.0-Linux-x86_64.tar.gz --strip 1 -C /usr/local
+	sudo tar xf cmake-3.12.0-Linux-x86_64.tar.gz --strip 1 -C /usr/local
 
 	# Compile and install Swig-3.0.12 from source.
 	wget https://github.com/swig/swig/archive/rel-3.0.12.tar.gz

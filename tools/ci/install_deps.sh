072b5920c4228d7dabd0cefc3b4e71b489b1b80f
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2018-07-30 23:57:36 +0200

    Use latest CMake in CI.

--- tools/ci/install_deps.sh
+++ tools/ci/install_deps.sh
@@ -33,6 +33,10 @@ pushd "$DEPS_DIR"
 			lua
 	fi
 
+	# Install CMake-3.12.0 from binary package.
+	wget https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.tar.gz
+	tar xf cmake-3.12.0-Linux-x86_64.tar.gz --strip 1 -C /usr/local
+
 	# Compile and install Swig-3.0.12 from source.
 	wget https://github.com/swig/swig/archive/rel-3.0.12.tar.gz
 	tar xf rel-3.0.12.tar.gz

6330be1d4b559e9737007f976ca34706227a7283
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2019-04-14 00:30:37 +0200

    Add Octave and R to build_linux.sh.

--- tools/ci/build_linux.sh
+++ tools/ci/build_linux.sh
@@ -6,17 +6,18 @@ ROOT_DIR="${SCRIPT_DIR}/../.."
 DIST_DIR="${SCRIPT_DIR}/dist"
 mkdir -p ${DIST_DIR}
 
-# Docker image repository.
+# Docker image repository name.
 REPOSITORY="tinyspline"
 
-APT_GET_CMDS=$(cat << END
+# Initial setup commands. Expects that 'apt-get' is present.
+SETUP_CMDS=$(cat << END
 RUN apt-get update && apt-get install -y --no-install-recommends cmake swig
 COPY . /tinyspline
 WORKDIR /tinyspline
 END
 )
 
-# Expects that the artifacts are located at /tinyspline/dist/
+# Expects that the artifacts are located in: /tinyspline/dist/
 COPY_ARTIFACTS_AND_DELETE() {
 	CONTAINER_ID=$(docker ps -aqf "name=${1}")
 	docker cp "${CONTAINER_ID}":/tinyspline/dist/. ${DIST_DIR}
@@ -25,34 +26,38 @@ COPY_ARTIFACTS_AND_DELETE() {
 }
 
 ################################# C#, D, Java #################################
-NAME="misc"
-docker build -t "${REPOSITORY}:${NAME}" -f - ${ROOT_DIR} <<-END
-	FROM buildpack-deps:stretch
-	${APT_GET_CMDS}
-	RUN apt-get install -y --no-install-recommends 	\
-		mono-mcs nuget \
-		dub \
-		default-jdk maven
-	END
-docker run --name "${NAME}" "${REPOSITORY}:${NAME}" \
-	/bin/bash -c "mkdir -p dist && cmake \
-	-DTINYSPLINE_ENABLE_CSHARP=True \
-	-DTINYSPLINE_ENABLE_DLANG=True \
-	-DTINYSPLINE_ENABLE_JAVA=True . && \
-	cmake --build . --target tinysplinecsharp && \
-		nuget pack && mv ./*.nupkg dist && \
-	dub build && tar cJf dist/tinysplinedlang.tar.xz dub && \
-	mvn package && mv ./target/*.jar dist"
-	COPY_ARTIFACTS_AND_DELETE ${NAME}
+BUILD_CSHARP_D_JAVA() {
+	NAME="csharp.d.java"
+	docker build -t "${REPOSITORY}:${NAME}" -f - ${ROOT_DIR} <<-END
+		FROM buildpack-deps:stretch
+		${SETUP_CMDS}
+		RUN apt-get install -y --no-install-recommends 	\
+			mono-mcs nuget \
+			dub \
+			default-jdk maven
+		END
+	docker run --name "${NAME}" "${REPOSITORY}:${NAME}" \
+		/bin/bash -c "mkdir -p dist && cmake \
+			-DTINYSPLINE_ENABLE_CSHARP=True \
+			-DTINYSPLINE_ENABLE_DLANG=True \
+			-DTINYSPLINE_ENABLE_JAVA=True . && \
+		cmake --build . --target tinysplinecsharp && \
+			nuget pack && mv ./*.nupkg dist && \
+		dub build && tar cJf dist/tinysplinedlang.tar.xz dub && \
+		mvn package && mv ./target/*.jar dist"
+		COPY_ARTIFACTS_AND_DELETE ${NAME}
+}
+
+BUILD_CSHARP_D_JAVA
 
 ##################################### Lua #####################################
 BUILD_LUA() {
 	NAME="lua${1}"
 	docker build -t "${REPOSITORY}:${NAME}" -f - ${ROOT_DIR} <<-END
 		FROM buildpack-deps:stretch
-		${APT_GET_CMDS}
+		${SETUP_CMDS}
 		RUN apt-get install -y --no-install-recommends \
-		luarocks liblua${1}-dev
+			luarocks liblua${1}-dev
 		END
 	docker run --name "${NAME}" "${REPOSITORY}:${NAME}" \
 		/bin/bash -c "cmake -DTINYSPLINE_ENABLE_LUA=True . && \
@@ -76,7 +81,7 @@ BUILD_PYTHON() {
 	NAME="python${1}"
 	docker build -t "${REPOSITORY}:${NAME}" -f - ${ROOT_DIR} <<-END
 		FROM python:${1}-stretch
-		${APT_GET_CMDS}
+		${SETUP_CMDS}
 		END
 	docker run --name "${NAME}" "${REPOSITORY}:${NAME}" \
 		/bin/bash -c "cmake -DTINYSPLINE_ENABLE_PYTHON=True . && \
@@ -88,3 +93,38 @@ BUILD_PYTHON 2.7
 BUILD_PYTHON 3.5
 BUILD_PYTHON 3.6
 BUILD_PYTHON 3.7
+
+################################## Octave, R ##################################
+BUILD_OCTAVE_R_UBUNTU() {
+	NAME="octave.r.ubuntu${1}"
+	docker build -t "${REPOSITORY}:${NAME}" -f - ${ROOT_DIR} <<-END
+		FROM ubuntu:${1}
+		RUN echo 'debconf debconf/frontend select Noninteractive' \
+			| debconf-set-selections
+		${SETUP_CMDS}
+		RUN apt-get install -y --no-install-recommends \
+			build-essential wget \
+			liboctave-dev octave \
+			r-base r-cran-rcpp
+		END
+	docker run --name "${NAME}" "${REPOSITORY}:${NAME}" \
+		/bin/bash -c "mkdir -p dist && cmake \
+			-DTINYSPLINE_ENABLE_OCTAVE=True \
+			-DTINYSPLINE_ENABLE_R=True . && \
+		cmake --build . --target tinysplineoctave && \
+			find ./lib -name '*octave*' \
+			| tar cJf dist/tinysplineoctave.utnubu.tar.xz -T - && \
+		cmake --build . --target tinyspliner && \
+			find ./lib -name 'tinyspliner*' -o -name '*.R' \
+			| tar cJf dist/tinyspliner.utnubu.tar.xz -T -"
+	COPY_ARTIFACTS_AND_DELETE ${NAME}
+	for file in "${DIST_DIR}/"*utnubu.tar.xz
+	do
+		if [[ "${file}" != *"ubuntu"* ]];then
+			mv $file ${file/utnubu/ubuntu-${1}}
+		fi
+	done
+}
+
+BUILD_OCTAVE_R_UBUNTU 16.04
+BUILD_OCTAVE_R_UBUNTU 18.04

9d921ca4acc1c5fd9d52a177d13ee59a70b0bfe5
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2019-09-19 13:56:10 +0200

    Add build.windows-x86_64.sh and bundle nupkg.

--- tools/ci/bundle.sh
+++ tools/ci/bundle.sh
@@ -14,12 +14,33 @@ if [ ! -d "${MACOSX_X86_64}" ]; then
 	echo "MacOSX (x86_64) is missing.  Aborting."
 	exit -1
 fi
+WINDOWS_X86_64="${SCRIPT_DIR}/build.windows-x86_64"
+if [ ! -d "${WINDOWS_X86_64}" ]; then
+	echo "Windows (x86_64) is missing.  Aborting."
+	exit -1
+fi
 
 ### Prepare output directory.
 OUTPUT="${SCRIPT_DIR}/tinyspline"
 mkdir -p "${OUTPUT}"
 
 ### Copy and merge files.
+# C#
+NUPKG_TMP_DIR="${SCRIPT_DIR}/nuget"
+find "${WINDOWS_X86_64}" -name '*.nupkg' -print0 | \
+	xargs -0 -I{} unzip -d "${NUPKG_TMP_DIR}" -o {}
+find "${MACOSX_X86_64}" -name '*.nupkg' -print0 | \
+	xargs -0 -I{} unzip -d "${NUPKG_TMP_DIR}" -o {}
+find "${LINUX_X86_64}" -name '*.nupkg' -print0 | \
+	xargs -0 -I{} unzip -d "${NUPKG_TMP_DIR}" -o {}
+# See https://github.com/NuGet/Home/issues/5956
+find  "${SCRIPT_DIR}/nuget/runtimes/" -name '*\%2B*' \
+	-exec /bin/sh -c 'mv -u $0 ${0/\%2B\%2B/++}' {} \;
+NUPKG_NAME=$( find "${LINUX_X86_64}" -name "*.nupkg" -exec basename {} \; )
+pushd ${NUPKG_TMP_DIR}
+	zip -r "${OUTPUT}/${NUPKG_NAME}" *
+popd
+
 # Lua
 find "${LINUX_X86_64}" -name '*.rock' -print0 | \
 	xargs -0 -I{} cp {} "${OUTPUT}"

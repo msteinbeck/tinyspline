e62855170452655927f2db3264c9a7f9bea47595
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2020-01-19 23:58:30 +0100

    Minor enhancement in sample test.

--- test/c/sample.c
+++ test/c/sample.c
@@ -9,37 +9,36 @@ void sample_num_1(CuTest *tc)
 	tsBSpline spline = ts_bspline_init();
 	tsReal *points = NULL;
 	size_t num;
+	tsReal dist;
 	tsStatus status;
 
+	tsReal ctrlp[12] = {
+		-1.75f, 1.0f,
+		-1.5f, -0.5f,
+		-1.5f,  0.f,
+		-1.25f, 0.5f
+	};
+
 	TS_TRY(try, status.code, &status)
+		/* Create spline with control points. */
 		TS_CALL(try, status.code, ts_bspline_new(
 			4, 2, 3, TS_CLAMPED, &spline, &status))
-
-		TS_CALL(try, status.code, ts_bspline_control_points(
-			&spline, &points, &status))
-		points[0]  = -1.75f; /* x0 */
-		points[1]  =  1.0f;  /* y0 */
-		points[2]  = -1.5f;  /* x1 */
-		points[3]  = -0.5f;  /* y1 */
-		points[4]  = -1.5f;  /* x2 */
-		points[5]  =  0.0f;  /* y2 */
-		points[6]  = -1.25f; /* x3 */
-		points[7]  =  0.5f;  /* y3 */
 		TS_CALL(try, status.code, ts_bspline_set_control_points(
-			&spline, points, &status));
-		free(points);
-		points = NULL;
+			&spline, ctrlp, &status));
 
+		/* Check sample with num = 1. */
 		TS_CALL(try, status.code, ts_bspline_sample(
 			&spline, 1, &points, &num, &status))
 		CuAssertTrue(tc, num == 1);
-		CuAssertDblEquals(tc, -1.75f, points[0], EPSILON);
-		CuAssertDblEquals(tc,  1.0f , points[1], EPSILON);
+		dist = ts_distance(points, ctrlp,
+			ts_bspline_dimension(&spline));
+		CuAssertDblEquals(tc, 0, dist, EPSILON);
 	TS_CATCH(status.code)
 		CuFail(tc, status.message);
 	TS_FINALLY
 		ts_bspline_free(&spline);
 		free(points);
+		points = NULL;
 	TS_END_TRY
 }
 
@@ -48,39 +47,41 @@ void sample_num_2(CuTest *tc)
 	tsBSpline spline = ts_bspline_init();
 	tsReal *points = NULL;
 	size_t num;
+	tsReal dist;
 	tsStatus status;
 
+	tsReal ctrlp[12] = {
+		-1.75f,  1.0f,
+		-1.5f,  -0.5f,
+		-1.5f,   0.f,
+		 1.25f, -0.5f
+	};
+
 	TS_TRY(try, status.code, &status)
+		/* Create spline with control points. */
 		TS_CALL(try, status.code, ts_bspline_new(
 			4, 2, 3, TS_CLAMPED, &spline, &status))
-
-		TS_CALL(try, status.code, ts_bspline_control_points(
-			&spline, &points, &status))
-		points[0]  = -1.75f; /* x0 */
-		points[1]  =  1.0f;  /* y0 */
-		points[2]  = -1.5f;  /* x1 */
-		points[3]  =  0.5f;  /* y1 */
-		points[4]  = -1.5f;  /* x2 */
-		points[5]  =  0.0f;  /* y2 */
-		points[6]  =  1.25f; /* x3 */
-		points[7]  = -0.5f;  /* y3 */
 		TS_CALL(try, status.code, ts_bspline_set_control_points(
-			&spline, points, &status));
-		free(points);
-		points = NULL;
+			&spline, ctrlp, &status));
 
+		/* Check sample with num = 2. */
 		TS_CALL(try, status.code, ts_bspline_sample(
 			&spline, 2, &points, &num, &status))
 		CuAssertTrue(tc, num == 2);
-		CuAssertDblEquals(tc, -1.75f, points[0], EPSILON);
-		CuAssertDblEquals(tc,  1.0f , points[1], EPSILON);
-		CuAssertDblEquals(tc,  1.25f, points[2], EPSILON);
-		CuAssertDblEquals(tc, -0.5f , points[3], EPSILON);
+		dist = ts_distance(points, ctrlp,
+			ts_bspline_dimension(&spline));
+		CuAssertDblEquals(tc, 0, dist, EPSILON);
+		dist = ts_distance(
+			points +   ts_bspline_dimension(&spline),
+			ctrlp  + 3*ts_bspline_dimension(&spline),
+			ts_bspline_dimension(&spline));
+		CuAssertDblEquals(tc, 0, dist, EPSILON);
 	TS_CATCH(status.code)
 		CuFail(tc, status.message);
 	TS_FINALLY
 		ts_bspline_free(&spline);
 		free(points);
+		points = NULL;
 	TS_END_TRY
 }
 
@@ -88,50 +89,56 @@ void sample_num_3(CuTest *tc)
 {
 	tsBSpline spline = ts_bspline_init();
 	tsDeBoorNet net = ts_deboornet_init();
-	tsReal *points = NULL, *result = NULL;
+	tsReal *middle = NULL, *points = NULL;
+	tsReal min, max, dist;
 	size_t num;
 	tsStatus status;
 
+	tsReal ctrlp[12] = {
+		-1.75f,  1.0f,
+		-1.5f,   0.5f,
+		-1.5f,   0.0f,
+		 1.25f, -0.5f
+	};
+
 	TS_TRY(try, status.code, &status)
+		/* Create spline with control points. */
 		TS_CALL(try, status.code, ts_bspline_new(
 			4, 2, 3, TS_CLAMPED, &spline, &status))
-
-		TS_CALL(try, status.code, ts_bspline_control_points(
-			&spline, &points, &status))
-		points[0]  = -1.75f; /* x0 */
-		points[1]  =  1.0f;  /* y0 */
-		points[2]  = -1.5f;  /* x1 */
-		points[3]  =  0.5f;  /* y1 */
-		points[4]  = -1.5f;  /* x2 */
-		points[5]  =  0.0f;  /* y2 */
-		points[6]  =  1.25f; /* x3 */
-		points[7]  = -0.5f;  /* y3 */
 		TS_CALL(try, status.code, ts_bspline_set_control_points(
-			&spline, points, &status));
-		free(points);
-		points = NULL;
+			&spline, ctrlp, &status));
+		ts_bspline_domain(&spline, &min, &max);
 
+		/* Eval middle point. */
 		TS_CALL(try, status.code, ts_bspline_eval(
-			&spline, 0.5f, &net, &status))
+			&spline, (max-min)/2.f, &net, &status))
 		TS_CALL(try, status.code, ts_deboornet_result(
-			&net, &result, &status))
+			&net, &middle, &status))
 
+		/* Check sample with num = 3. */
 		TS_CALL(try, status.code, ts_bspline_sample(
 			&spline, 3, &points, &num, &status))
 		CuAssertTrue(tc, num == 3);
-		CuAssertDblEquals(tc, -1.75f, points[0], EPSILON);
-		CuAssertDblEquals(tc,  1.0f , points[1], EPSILON);
-		CuAssertDblEquals(tc, result[0], points[2], EPSILON);
-		CuAssertDblEquals(tc, result[1], points[3], EPSILON);
-		CuAssertDblEquals(tc,  1.25f, points[4], EPSILON);
-		CuAssertDblEquals(tc, -0.5f , points[5], EPSILON);
+		dist = ts_distance(points, ctrlp,
+			ts_bspline_dimension(&spline));
+		CuAssertDblEquals(tc, 0, dist, EPSILON);
+		dist = ts_distance(points + ts_bspline_dimension(&spline),
+			middle, ts_bspline_dimension(&spline));
+		CuAssertDblEquals(tc, 0, dist, EPSILON);
+		dist = ts_distance(
+			points + 2*ts_bspline_dimension(&spline),
+			ctrlp  + 3*ts_bspline_dimension(&spline),
+			ts_bspline_dimension(&spline));
+		CuAssertDblEquals(tc, 0, dist, EPSILON);
 	TS_CATCH(status.code)
 		CuFail(tc, status.message);
 	TS_FINALLY
 		ts_bspline_free(&spline);
-		free(points);
 		ts_deboornet_free(&net);
-		free(result);
+		free(middle);
+		middle = NULL;
+		free(points);
+		points = NULL;
 	TS_END_TRY
 }
 

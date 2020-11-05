86627d7e58b3733cf4b1a040634640138e202b00
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2020-11-05 21:52:11 +0100

    Build python and lua packages for windows.

--- tools/ci/build.sh
+++ tools/ci/build.sh
@@ -52,10 +52,75 @@ docker run \
 				-DCMAKE_BUILD_TYPE=Release \
 				-DTINYSPLINE_ENABLE_LUA=True \
 				-DLUA_INCLUDE_DIR=/usr/include/lua5.1 \
-				-DLUA_LIBRARY=/usr/lib/x86_64-linux-gnu/liblua5.1.so && \
+				-DLUA_LIBRARY=/usr/lib/x86_64-linux-gnu/liblua5.1.so \
+				-DTINYSPLINE_ENABLE_PYTHON=True \
+				-DPYTHON_INCLUDE_DIR=/usr/include/python2.7 \
+				-DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so && \
 			sed -i '/dependencies/,/}/d' *.rockspec && \
 			luarocks make --pack-binary-rock && \
 				chown $(id -u):$(id -g) *.rock && \
-				cp -a *.rock ${STORAGE}/linux64"
+				cp -a *.rock ${STORAGE}/linux64 && \
+			python2 setup.py bdist_wheel && \
+				chown $(id -u):$(id -g) dist/*.whl && \
+				cp -a dist/*.whl ${STORAGE}/linux64 && \
+		rm -rf ..?* .[!.]* * && \
+			cmake .. \
+				-DCMAKE_BUILD_TYPE=Release \
+				-DTINYSPLINE_ENABLE_LUA=True \
+				-DLUA_INCLUDE_DIR=/usr/include/lua5.2 \
+				-DLUA_LIBRARY=/usr/lib/x86_64-linux-gnu/liblua5.2.so \
+				-DTINYSPLINE_ENABLE_PYTHON=True \
+				-DPYTHON_INCLUDE_DIR=/usr/include/python3.5m \
+				-DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.5m.so && \
+			sed -i '/dependencies/,/}/d' *.rockspec && \
+			luarocks make --pack-binary-rock && \
+				chown $(id -u):$(id -g) *.rock && \
+				cp -a *.rock ${STORAGE}/linux64 && \
+			python3 setup.py bdist_wheel && \
+				chown $(id -u):$(id -g) dist/*.whl && \
+				cp -a dist/*.whl ${STORAGE}/linux64 && \
+		rm -rf ..?* .[!.]* * && \
+			cmake .. \
+				-DCMAKE_BUILD_TYPE=Release \
+				-DTINYSPLINE_ENABLE_LUA=True \
+				-DLUA_INCLUDE_DIR=/usr/include/lua5.3 \
+				-DLUA_LIBRARY=/usr/lib/x86_64-linux-gnu/liblua5.3.so \
+				-DTINYSPLINE_ENABLE_PYTHON=True \
+				-DPYTHON_INCLUDE_DIR=/opt/linux/python36/include/python3.6m \
+				-DPYTHON_LIBRARY=/opt/linux/python36/lib/libpython3.6m.so && \
+			sed -i '/dependencies/,/}/d' *.rockspec && \
+			luarocks make --pack-binary-rock && \
+				chown $(id -u):$(id -g) *.rock && \
+				cp -a *.rock ${STORAGE}/linux64 && \
+			/opt/linux/python36/bin/python3 setup.py bdist_wheel && \
+				chown $(id -u):$(id -g) dist/*.whl && \
+				cp -a dist/*.whl ${STORAGE}/linux64 && \
+		rm -rf ..?* .[!.]* * && \
+			cmake .. \
+				-DCMAKE_BUILD_TYPE=Release \
+				-DTINYSPLINE_ENABLE_PYTHON=True \
+				-DPYTHON_INCLUDE_DIR=/opt/linux/python37/include/python3.7m \
+				-DPYTHON_LIBRARY=/opt/linux/python37/lib/libpython3.7m.so && \
+			/opt/linux/python37/bin/python3 setup.py bdist_wheel && \
+				chown $(id -u):$(id -g) dist/*.whl && \
+				cp -a dist/*.whl ${STORAGE}/linux64 && \
+		rm -rf ..?* .[!.]* * && \
+			cmake .. \
+				-DCMAKE_BUILD_TYPE=Release \
+				-DTINYSPLINE_ENABLE_PYTHON=True \
+				-DPYTHON_INCLUDE_DIR=/opt/linux/python38/include/python3.8 \
+				-DPYTHON_LIBRARY=/opt/linux/python38/lib/libpython3.8.so && \
+			/opt/linux/python38/bin/python3 setup.py bdist_wheel && \
+				chown $(id -u):$(id -g) dist/*.whl && \
+				cp -a dist/*.whl ${STORAGE}/linux64 && \
+		rm -rf ..?* .[!.]* * && \
+			cmake .. \
+				-DCMAKE_BUILD_TYPE=Release \
+				-DTINYSPLINE_ENABLE_PYTHON=True \
+				-DPYTHON_INCLUDE_DIR=/opt/linux/python39/include/python3.9 \
+				-DPYTHON_LIBRARY=/opt/linux/python39/lib/libpython3.9.so && \
+			/opt/linux/python39/bin/python3 setup.py bdist_wheel && \
+				chown $(id -u):$(id -g) dist/*.whl && \
+				cp -a dist/*.whl ${STORAGE}/linux64"
 
 docker rmi ${IMAGE_NAME}

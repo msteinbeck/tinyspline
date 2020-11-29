fb883ae5c730300210a029aba9961e4715aac9db
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2020-11-30 00:26:10 +0100

    Update script for building docs.

---
+++ tools/ci/docs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$( cd "$(dirname "$0")"; pwd -P)
+ROOT_DIR="${SCRIPT_DIR}/../.."
+SRC_DIR="/tinyspline"
+IMAGE_NAME="tinyspline/docs:latest"
+STORAGE="/storage"
+VOLUME="${SCRIPT_DIR}/docs"
+mkdir -p "${VOLUME}"
+
+docker build -t ${IMAGE_NAME} -f - "${ROOT_DIR}" <<-END
+	FROM tinyspline/build-deps:latest
+	COPY . ${SRC_DIR}
+	WORKDIR /
+END
+
+docker run \
+	--rm \
+	--volume "${VOLUME}:${STORAGE}" \
+	${IMAGE_NAME} /bin/bash -c \
+	"mkdir build && pushd build && \
+		cmake ${SRC_DIR} && \
+		cmake --build . --target docs && \
+		chown -R $(id -u):$(id -g) docs && \
+		cp -a docs/doxygen ${STORAGE}/doxygen && \
+	popd"
+
+docker rmi ${IMAGE_NAME}

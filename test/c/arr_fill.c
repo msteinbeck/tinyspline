832ab5bfadf698324a751aa9dd58dbe0ab67c5e4
Marcel Steinbeck <marcel@pc.lan>
2021-05-24 12:51:14 +0200

    Fix warnings in test file arr_fill and decompose composite tests.

--- test/c/arr_fill.c
+++ test/c/arr_fill.c
@@ -3,7 +3,7 @@
 
 #define EPSILON 0.0001
 
-void arr_fill_test_standard_case(CuTest *tc)
+void arr_fill_size10_negative_integer(CuTest *tc)
 {
 	tsReal arr[10];
 	size_t i;
@@ -12,50 +12,75 @@ void arr_fill_test_standard_case(CuTest *tc)
 	for (i = 0; i < 10; i++)
 		arr[i] = (tsReal) i;
 
-/* ================================ When (1) =============================== */
+/* ================================= When ================================== */
 	ts_arr_fill(arr, 10, (tsReal) -3.0);
-/* ================================ Then (1) =============================== */
-	for (i = 0; i < 10; i++)
-		CuAssertDblEquals(tc, -3.0, arr[i], EPSILON);
 
-/* ================================ When (2) =============================== */
-	ts_arr_fill(arr, 10, (tsReal) 10.586);
-/* ================================ Then (2) =============================== */
+/* ================================= Then ================================== */
 	for (i = 0; i < 10; i++)
-		CuAssertDblEquals(tc, 10.586, arr[i], EPSILON);
+		CuAssertDblEquals(tc, -3.0, (double) arr[i], EPSILON);
 }
 
-void arr_fill_test_subset(CuTest *tc)
+void arr_fill_size75_real(CuTest *tc)
 {
-	tsReal arr[10];
+	tsReal arr[75];
 	size_t i;
 
 /* ================================= Given ================================= */
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 75; i++)
 		arr[i] = (tsReal) i;
 
-/* ================================ When (1) =============================== */
-	ts_arr_fill(arr, 5, (tsReal) -15.0);
-/* ================================ Then (1) =============================== */
-	for (i = 0; i < 5; i++)
-		CuAssertDblEquals(tc, -15.0, arr[i], EPSILON);
-	for (i = 5; i < 10; i++)
-		CuAssertDblEquals(tc, i, arr[i], EPSILON);
+/* ================================= When ================================== */
+	ts_arr_fill(arr, 75, (tsReal) 10.586);
 
-/* ================================ When (2) =============================== */
-	ts_arr_fill(arr, 1, (tsReal) -20);
-/* ================================ Then (2) =============================== */
-	CuAssertDblEquals(tc, -20, arr[0], EPSILON);
-	for (i = 1; i < 5; i++)
-		CuAssertDblEquals(tc, -15.0, arr[i], EPSILON);
-	for (i = 5; i < 10; i++)
-		CuAssertDblEquals(tc, i, arr[i], EPSILON);
+/* ================================= Then ================================== */
+	for (i = 0; i < 75; i++)
+		CuAssertDblEquals(tc, 10.586, (double) arr[i], EPSILON);
+}
+
+void arr_fill_subset(CuTest *tc)
+{
+	tsReal arr[50];
+	size_t i;
+
+/* ================================= Given ================================= */
+	for (i = 0; i < 50; i++)
+		arr[i] = (tsReal) i;
+
+/* ================================= When ================================== */
+	ts_arr_fill(arr, 5, (tsReal) 15.0);
+
+/* ================================= Then ================================== */
+	for (i = 0; i < 5; i++)
+		CuAssertDblEquals(tc, 15.0, (double) arr[i], EPSILON);
+	for (i = 5; i < 50; i++)
+		CuAssertDblEquals(tc, (double) i, (double) arr[i], EPSILON);
+}
+
+void arr_fill_subset_with_offset(CuTest *tc)
+{
+	tsReal arr[13];
+	size_t i;
+
+/* ================================= Given ================================= */
+	for (i = 0; i < 13; i++)
+		arr[i] = (tsReal) i;
+
+/* ================================= When ================================== */
+	ts_arr_fill(arr + 10, 3, (tsReal) 0.0);
+
+/* ================================= Then ================================== */
+	for (i = 0; i < 10; i++)
+		CuAssertDblEquals(tc, (double) i, (double) arr[i], EPSILON);
+	for (i = 10; i < 13; i++)
+		CuAssertDblEquals(tc, 0.0, (double) arr[i], EPSILON);
 }
 
 CuSuite* get_arr_fill_suite()
 {
 	CuSuite* suite = CuSuiteNew();
-	SUITE_ADD_TEST(suite, arr_fill_test_standard_case);
-	SUITE_ADD_TEST(suite, arr_fill_test_subset);
+	SUITE_ADD_TEST(suite, arr_fill_size10_negative_integer);
+	SUITE_ADD_TEST(suite, arr_fill_size75_real);
+	SUITE_ADD_TEST(suite, arr_fill_subset);
+	SUITE_ADD_TEST(suite, arr_fill_subset_with_offset);
 	return suite;
 }

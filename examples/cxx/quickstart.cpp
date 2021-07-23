61594a6c08dd32ebcdefbeada7dd84988d0a29f5
Marcel Steinbeck <marcel@pc.lan>
2021-07-23 11:21:22 +0200

    Fix MSVC warnings.

--- examples/cxx/quickstart.cpp
+++ examples/cxx/quickstart.cpp
@@ -10,31 +10,31 @@ int main(int argc, char **argv)
 
 	// Setup control points.
 	std::vector<tinyspline::real> ctrlp = spline.controlPoints();
-	ctrlp[0]  = -1.75; // x0
-	ctrlp[1]  = -1.0;  // y0
-	ctrlp[2]  = -1.5;  // x1
-	ctrlp[3]  = -0.5;  // y1
-	ctrlp[4]  = -1.5;  // x2
-	ctrlp[5]  =  0.0;  // y2
-	ctrlp[6]  = -1.25; // x3
-	ctrlp[7]  =  0.5;  // y3
-	ctrlp[8]  = -0.75; // x4
-	ctrlp[9]  =  0.75; // y4
-	ctrlp[10] =  0.0;  // x5
-	ctrlp[11] =  0.5;  // y5
-	ctrlp[12] =  0.5;  // x6
-	ctrlp[13] =  0.0;  // y6
+	ctrlp[0]  = (tsReal) -1.75; // x0
+	ctrlp[1]  = (tsReal) -1.0;  // y0
+	ctrlp[2]  = (tsReal) -1.5;  // x1
+	ctrlp[3]  = (tsReal) -0.5;  // y1
+	ctrlp[4]  = (tsReal) -1.5;  // x2
+	ctrlp[5]  = (tsReal)  0.0;  // y2
+	ctrlp[6]  = (tsReal) -1.25; // x3
+	ctrlp[7]  = (tsReal)  0.5;  // y3
+	ctrlp[8]  = (tsReal) -0.75; // x4
+	ctrlp[9]  = (tsReal)  0.75; // y4
+	ctrlp[10] = (tsReal)  0.0;  // x5
+	ctrlp[11] = (tsReal)  0.5;  // y5
+	ctrlp[12] = (tsReal)  0.5;  // x6
+	ctrlp[13] = (tsReal)  0.0;  // y6
 	spline.setControlPoints(ctrlp);
 
 	// Evaluate `spline` at u = 0.4 using 'eval'.
-	std::vector<tinyspline::real> result = spline.eval(0.4).result();
+	std::vector<tinyspline::real> result = spline.eval(0.4f).result();
 	std::cout << "x = " << result[0] << ", y = " << result[1] << std::endl;
 
 	// Derive `spline` and subdivide it into a sequence of Bezier curves.
 	tinyspline::BSpline beziers = spline.derive().toBeziers();
 
 	// Evaluate `beziers` at u = 0.3 using '()' instead of 'eval'.
-	result = beziers(0.3).result();
+	result = beziers((tsReal) 0.3f).result();
 	std::cout << "x = " << result[0] << ", y = " << result[1] << std::endl;
 
 	return 0;

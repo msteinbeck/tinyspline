2f9ef9e5f6ed3ee3da77413988595c36e2f8a9e0
Marcel Steinbeck <marcel@pc.lan>
2021-12-22 10:21:04 +0100

    Add another tension test.

--- test/c/tension.c
+++ test/c/tension.c
@@ -1,6 +1,7 @@
 #include <testutils.h>
 
-void tension_beta_0(CuTest *tc)
+void
+tension_beta_0(CuTest *tc)
 {
 	___SETUP___
 	tsBSpline spline = ts_bspline_init();
@@ -30,9 +31,38 @@ void tension_beta_0(CuTest *tc)
 	ts_bspline_free(&line);
 }
 
+void
+tension_beta_1(CuTest *tc)
+{
+	___SETUP___
+	tsBSpline spline = ts_bspline_init();
+	tsBSpline expected = ts_bspline_init();
+	size_t i;
+
+	___GIVEN___
+	C(ts_bspline_new_with_control_points(
+		4, 2, 3, TS_CLAMPED, &spline, &status,
+		200.0, 100.0, /* P1 */
+		360.0, 40.0,  /* P2 */
+		410.0, 20.0,  /* P3 */
+		380.0, 50.0)) /* P4 */
+	C(ts_bspline_copy(&spline, &expected, &status))
+
+	___WHEN___
+	C(ts_bspline_tension(&spline, 1.f, &spline, &status))
+
+	___THEN___
+	assert_equal_shape(tc, &spline, &expected);
+
+	___TEARDOWN___
+	ts_bspline_free(&spline);
+	ts_bspline_free(&expected);
+}
+
 CuSuite* get_tension_suite()
 {
 	CuSuite* suite = CuSuiteNew();
 	SUITE_ADD_TEST(suite, tension_beta_0);
+	SUITE_ADD_TEST(suite, tension_beta_1);
 	return suite;
 }

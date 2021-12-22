2eebd37499d6431b9d8a0439593515d95aef0bfd
Marcel Steinbeck <marcel@pc.lan>
2021-12-22 11:44:13 +0100

    Add python script to interpolate points with Catmull-Rom splines.

---
+++ test/c/res/catmullrom.py
@@ -0,0 +1,86 @@
+# This script is based on:
+#     https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
+# It is licensed under the terms of the Creative Commons Attribution-ShareAlike License:
+#     https://en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License
+
+# This script serves as a source of truth for interpolating points with
+# Catmull-Rom splines. The interpolated points are printed to stdout.
+
+import numpy
+import matplotlib.pyplot as plt
+
+def CatmullRomSpline(P0, P1, P2, P3, nPoints=100):
+    """
+    P0, P1, P2, and P3 should be (x,y) point pairs that define the Catmull-Rom spline.
+    nPoints is the number of points to include in this curve segment.
+    """
+    # Convert the points to numpy so that we can do array multiplication
+    P0, P1, P2, P3 = map(numpy.array, [P0, P1, P2, P3])
+
+    # Parametric constant: 0.5 for the centripetal spline, 0.0 for the uniform spline, 1.0 for the chordal spline.
+    alpha = 0.5
+    # Premultiplied power constant for the following tj() function.
+    alpha = alpha/2
+    def tj(ti, Pi, Pj):
+        xi, yi = Pi
+        xj, yj = Pj
+        return ((xj-xi)**2 + (yj-yi)**2)**alpha + ti
+
+    # Calculate t0 to t4
+    t0 = 0
+    t1 = tj(t0, P0, P1)
+    t2 = tj(t1, P1, P2)
+    t3 = tj(t2, P2, P3)
+
+    # Only calculate points between P1 and P2
+    t = numpy.linspace(t1, t2, nPoints)
+
+    # Reshape so that we can multiply by the points P0 to P3
+    # and get a point for each value of t.
+    t = t.reshape(len(t), 1)
+    #print(t)
+    A1 = (t1-t)/(t1-t0)*P0 + (t-t0)/(t1-t0)*P1
+    A2 = (t2-t)/(t2-t1)*P1 + (t-t1)/(t2-t1)*P2
+    A3 = (t3-t)/(t3-t2)*P2 + (t-t2)/(t3-t2)*P3
+    #print(A1)
+    #print(A2)
+    #print(A3)
+    B1 = (t2-t)/(t2-t0)*A1 + (t-t0)/(t2-t0)*A2
+    B2 = (t3-t)/(t3-t1)*A2 + (t-t1)/(t3-t1)*A3
+
+    C = (t2-t)/(t2-t1)*B1 + (t-t1)/(t2-t1)*B2
+    return C
+
+def CatmullRomChain(P):
+    """
+    Calculate Catmull-Rom for a chain of points and return the combined curve.
+    """
+    sz = len(P)
+
+    # The curve C will contain an array of (x, y) points.
+    C = []
+    for i in range(sz-3):
+        c = CatmullRomSpline(P[i], P[i+1], P[i+2], P[i+3])
+        C.extend(c)
+
+    return C
+
+# Define a set of points for curve to go through
+Points = [[0, 1.5], [2, 2], [3, 1], [4, 0.5], [5, 1], [6, 2], [7, 3]]
+
+# Calculate the Catmull-Rom splines through the points
+c = CatmullRomChain(Points)
+
+# Convert the Catmull-Rom curve points into x and y arrays and plot
+x, y = zip(*c)
+plt.plot(x, y)
+
+# Plot the control points
+px, py = zip(*Points)
+plt.plot(px, py, 'or')
+
+# Print x, y coordinates
+xy = "\n".join("{}, {},".format(a, b) for a, b in zip(x, y))
+print(xy)
+
+plt.show()

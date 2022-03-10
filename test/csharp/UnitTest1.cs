1e62a82c933f70c86358463237d74e1eef6c76c3
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2022-03-10 10:23:01 +0100

    Add basic C# test project.

---
+++ test/csharp/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace csharp;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

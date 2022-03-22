38fd995f3ab7ec807f071fcc5b10c142be2d3c2b
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2022-03-22 12:58:20 +0100

    Update release scripts.

--- tools/release/maven.sh
+++ tools/release/maven.sh
@@ -3,20 +3,18 @@ set -e
 
 SCRIPT_DIR=$( cd "$(dirname "$0")"; pwd -P)
 TMP_DIR="${SCRIPT_DIR}/maven"
-ZIP_NAME="tinyspline-java.zip"
-ZIP_FILE="${SCRIPT_DIR}/${ZIP_NAME}"
+ZIP_FILE="${SCRIPT_DIR}/tinyspline/tinyspline-java.zip"
 SIGNED_ZIP_FILE="${SCRIPT_DIR}/tinyspline-java-signed.zip"
-URL="https://github.com/msteinbeck/tinyspline/releases/download/snapshot/${ZIP_NAME}"
 
-wget -O "${ZIP_FILE}" "${URL}"
 unzip -d "${TMP_DIR}" -o "${ZIP_FILE}"
 pushd "${TMP_DIR}"
-	for file in *
-	do
-		gpg2 --no-tty \
-			--local-user 06B668EB \
-			--detach-sign \
-			--output "${file}.asc" "${file}"
-	done
-	zip -r "${SIGNED_ZIP_FILE}" *
+    for file in *
+    do
+        gpg2 --no-tty \
+             --local-user 1BA70C7A62392ED9372845302DBCE0097DC4D259 \
+             --detach-sign \
+             --output "${file}.asc" "${file}"
+    done
+    zip -r "${SIGNED_ZIP_FILE}" *
 popd
+rm -r ${TMP_DIR}
---
+++ tools/release/pypi.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+SCRIPT_DIR=$( cd "$(dirname "$0")"; pwd -P)
+TMP_DIR="${SCRIPT_DIR}/pypi"
+WHL_DIR="${SCRIPT_DIR}/tinyspline"
+
+mkdir -p ${TMP_DIR}
+for file in ${WHL_DIR}/*.whl; do
+    BASENAME=$( basename "${file}" )
+    cp "${file}" "${TMP_DIR}/${BASENAME/linux/manylinux2010}"
+done

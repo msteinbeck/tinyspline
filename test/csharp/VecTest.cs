46eb26332dd37fc8fb5c8f52f11f7f5f217d955f
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2022-04-01 18:35:07 +0200

    Add C# unit tests for `values`.

---
+++ test/csharp/VecTest.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TinySpline;
+
+namespace csharp;
+
+public class VecTest
+{
+    [Test]
+    public void TestValuesVec2()
+    {
+        // Given
+        Vec2 vec = new Vec2(1, 2);
+
+        // When
+        IList<double> values = vec.Values;
+
+        // Then
+        Assert.That(values, Is.EqualTo(new List<double> { 1, 2 })
+              .Within(tinysplinecsharp.TS_KNOT_EPSILON));
+    }
+
+    [Test]
+    public void TestValuesVec3()
+    {
+        // Given
+        Vec3 vec = new Vec3(3, 2, 1);
+
+        // When
+        IList<double> values = vec.Values;
+
+        // Then
+        Assert.That(values, Is.EqualTo(new List<double> { 3, 2, 1 })
+              .Within(tinysplinecsharp.TS_KNOT_EPSILON));
+    }
+
+    [Test]
+    public void TestValuesVec4()
+    {
+        // Given
+        Vec4 vec = new Vec4(1, 3, 2, 4);
+
+        // When
+        IList<double> values = vec.Values;
+
+        // Then
+        Assert.That(values, Is.EqualTo(new List<double> { 1, 3, 2, 4 })
+              .Within(tinysplinecsharp.TS_KNOT_EPSILON));
+    }
+}

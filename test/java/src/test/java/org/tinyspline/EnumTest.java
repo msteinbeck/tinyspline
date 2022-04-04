99a0395ba5f7465c72a738aadeb970f22c0d7bb3
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2022-04-04 11:43:55 +0200

    Add VecTest to Java.

--- test/java/src/test/java/org/tinyspline/EnumTest.java
+++ test/java/src/test/java/org/tinyspline/EnumTest.java
@@ -1,6 +1,5 @@
 package org.tinyspline;
 
-import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
---
+++ test/java/src/test/java/org/tinyspline/VecTest.java
@@ -0,0 +1,54 @@
+package org.tinyspline;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.data.Offset.offset;
+import static org.tinyspline.tinysplinejavaConstants.TS_KNOT_EPSILON;
+
+public class VecTest {
+
+    @Test
+    public void testValuesVec2() {
+        // Given
+        Vec2 vec = new Vec2(1, 2);
+
+        // When
+        List<Double> values = vec.getValues();
+
+        // Then
+        assertThat(values.stream().mapToDouble(Double::doubleValue).toArray())
+                .containsExactly(new double[] { 1.0, 2.0 },
+                        offset(TS_KNOT_EPSILON));
+    }
+
+    @Test
+    public void testValuesVec3() {
+        // Given
+        Vec3 vec = new Vec3(3, 2, 1);
+
+        // When
+        List<Double> values = vec.getValues();
+
+        // Then
+        assertThat(values.stream().mapToDouble(Double::doubleValue).toArray())
+                .containsExactly(new double[] { 3.0, 2.0, 1.0 },
+                        offset(TS_KNOT_EPSILON));
+    }
+
+    @Test
+    public void testValuesVec4() {
+        // Given
+        Vec4 vec = new Vec4(1, 3, 2, 4);
+
+        // When
+        List<Double> values = vec.getValues();
+
+        // Then
+        assertThat(values.stream().mapToDouble(Double::doubleValue).toArray())
+                .containsExactly(new double[] { 1.0, 3.0, 2.0, 4.0 },
+                        offset(TS_KNOT_EPSILON));
+    }
+}

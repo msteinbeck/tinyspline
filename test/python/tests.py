f90df44ad39d7a07073e9226278a783bef028c54
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2022-04-27 23:26:50 +0200

    Update python tests.

--- test/python/tests.py
+++ test/python/tests.py
@@ -1,46 +1,117 @@
 import unittest
 from tinyspline import *
 
+
+
+def assertKnotsAlmostEqual(exp, act, tc):
+    if len(exp) != len(act):
+        tc.fail("length does not match")
+    for i in range(len(exp)):
+        tc.assertAlmostEqual(exp[i], act[i], delta=TS_KNOT_EPSILON)
+
+
+
+class TestEnums(unittest.TestCase):
+
+    def testOpened(self):
+        # Given
+        spline = BSpline(7, 2, 3, BSpline.Opened)
+
+        # When
+
+        # Then
+        expected = [ 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 ]
+        assertKnotsAlmostEqual(expected, spline.knots, self)
+
+
+
+class TestFrames(unittest.TestCase):
+
+    def assertOrthogonal(self, v1, v2):
+        angle = v1.angle(v2)
+        self.assertAlmostEqual(90, angle, delta=TS_POINT_EPSILON)
+
+    def testVectors(self):
+        # Given
+        spline = BSpline(7, 2)
+        spline.control_points = [
+            120, 100, # P1
+            270, 40,  # P2
+            370, 490, # P3
+            590, 40,  # P4
+            570, 490, # P5
+            420, 480, # P6
+            220, 500  # P7
+            ]
+        knots = spline.uniform_knot_seq(100)
+
+        # When
+        frames = spline.compute_rmf(knots)
+
+        # Then
+        for i in range(len(knots)):
+            frame = frames.at(i)
+            knot = knots[i]
+
+            pos = spline(knot).result_vec3().distance(frame.position)
+            self.assertAlmostEqual(0, pos, delta=TS_POINT_EPSILON)
+
+            tan = spline.derive()(knot).result_vec3().normalize().distance(frame.tangent)
+            self.assertAlmostEqual(0, tan, delta=TS_POINT_EPSILON)
+
+            self.assertOrthogonal(frame.normal, frame.tangent)
+            self.assertOrthogonal(frame.normal, frame.binormal)
+            self.assertOrthogonal(frame.binormal, frame.tangent)
+
+
+
 class TestInterpolation(unittest.TestCase):
 
-    def test_cubic_natural(self):
-        spline = BSpline.interpolate_cubic_natural(
-            [1.0, -1.0,
-             -1.0, 2.0,
-             1.0, 4.0,
-             4.0, 3.0,
-             7.0, 5.0], 2)
-        ctrlp = spline.control_points
+    def assertEquals(self, vec, x, y):
+        dist = vec.distance(Vec2(x, y))
+        self.assertAlmostEqual(0, dist, delta=TS_POINT_EPSILON)
 
-        def assert_equals(pts, idx, x, y):
-            a = Vec3(pts[idx * 2], pts[idx * 2 + 1], 0)
-            b = Vec3(x, y, 0)
-            dist = (a - b).magnitude()
-            self.assertAlmostEqual(0.0, dist, delta=0.0001)
+    def testCubicNatural(self):
+        # Given
+        points = [
+            1.0, -1.0, # P1
+            -1.0, 2.0, # P2
+            1.0, 4.0,  # P3
+            4.0, 3.0,  # P4
+            7.0, 5.0   # P5
+            ]
+
+        # When
+        spline = BSpline.interpolate_cubic_natural(points, 2)
+
+        # Then
+        self.assertEqual(16, spline.num_control_points)
 
         # First bezier segment.
-        assert_equals(ctrlp, 0, 1.0, -1.0)
-        assert_equals(ctrlp, 1, 0.0, 0.0)
-        assert_equals(ctrlp, 2, -1.0, 1.0)
-        assert_equals(ctrlp, 3, -1.0, 2.0)
+        self.assertEquals(spline.control_point_vec_2at(0), 1.0, -1.0)
+        self.assertEquals(spline.control_point_vec_2at(1), 0.0, 0.0)
+        self.assertEquals(spline.control_point_vec_2at(2), -1.0, 1.0)
+        self.assertEquals(spline.control_point_vec_2at(3), -1.0, 2.0)
 
         # Second bezier segment.
-        assert_equals(ctrlp, 4, -1.0, 2.0)
-        assert_equals(ctrlp, 5, -1.0, 3.0)
-        assert_equals(ctrlp, 6, 0.0, 4.0)
-        assert_equals(ctrlp, 7, 1.0, 4.0)
+        self.assertEquals(spline.control_point_vec_2at(4), -1.0, 2.0)
+        self.assertEquals(spline.control_point_vec_2at(5), -1.0, 3.0)
+        self.assertEquals(spline.control_point_vec_2at(6), 0.0, 4.0)
+        self.assertEquals(spline.control_point_vec_2at(7), 1.0, 4.0)
 
         # Third bezier segment.
-        assert_equals(ctrlp, 8, 1.0, 4.0)
-        assert_equals(ctrlp, 9, 2.0, 4.0)
-        assert_equals(ctrlp, 10, 3.0, 3.0)
-        assert_equals(ctrlp, 11, 4.0, 3.0)
+        self.assertEquals(spline.control_point_vec_2at(8), 1.0, 4.0)
+        self.assertEquals(spline.control_point_vec_2at(9), 2.0, 4.0)
+        self.assertEquals(spline.control_point_vec_2at(10), 3.0, 3.0)
+        self.assertEquals(spline.control_point_vec_2at(11), 4.0, 3.0)
 
         # Forth bezier segment.
-        assert_equals(ctrlp, 12, 4.0, 3.0)
-        assert_equals(ctrlp, 13, 5.0, 3.0)
-        assert_equals(ctrlp, 14, 6.0, 4.0)
-        assert_equals(ctrlp, 15, 7.0, 5.0)
+        self.assertEquals(spline.control_point_vec_2at(12), 4.0, 3.0)
+        self.assertEquals(spline.control_point_vec_2at(13), 5.0, 3.0)
+        self.assertEquals(spline.control_point_vec_2at(14), 6.0, 4.0)
+        self.assertEquals(spline.control_point_vec_2at(15), 7.0, 5.0)
+
+
 
 if __name__ == '__main__':
     unittest.main()

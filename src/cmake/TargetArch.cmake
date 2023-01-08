c7051687ef0eaaee0d9ddb432690e123bb1cc59d
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2023-01-08 23:42:17 +0100

    Reformat TargetArch.cmake

--- src/cmake/TargetArch.cmake
+++ src/cmake/TargetArch.cmake
@@ -1,3 +1,11 @@
+# Detects the target architecture.
+# ~~~
+#
+# This module is based on the work of the Petroules Corporation:
+# https://github.com/axr/solar-cmake/blob/master/TargetArch.cmake
+#
+# License: ======================================================================
+#
 # Copyright (c) 2012 Petroules Corporation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -20,69 +28,77 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+#
+# ===============================================================================
+#
+# See: https://github.com/axr/solar-cmake/blob/master/TargetArch.cmake for more
+# details.
+# ~~~
 
-# Based on the Qt 5 processor detection code, so should be very accurate
-# https://qt.gitorious.org/qt/qtbase/blobs/master/src/corelib/global/qprocessordetection.h
-# Currently handles arm (v5, v6, v7), x86 (32/64), ia64, and ppc (32/64)
-
-# Regarding POWER/PowerPC, just as is noted in the Qt source,
-# "There are many more known variants/revisions that we do not handle/detect."
-
-# This script was adapted to the characteristics of TinySpline's build system.
-
-set(archdetect_c_code "
-#if defined(__aarch64__) || defined(_M_ARM64)
-	#error cmake_ARCH arm64
-#elif defined(__arm__) || defined(__TARGET_ARCH_ARM) || defined(_M_ARM)
-	#error cmake_ARCH arm
-#elif defined(__i386) || defined(__i386__) || defined(_M_IX86)
-	#error cmake_ARCH x86
-#elif defined(__x86_64) || defined(__x86_64__) || defined(__amd64) \\
-	|| defined(_M_X64)
-	#error cmake_ARCH x86_64
-#elif defined(__ia64) || defined(__ia64__) || defined(_M_IA64)
-	#error cmake_ARCH ia64
+set(archdetect_c_code
+    "
+#if   defined(__aarch64__) \\
+   || defined(_M_ARM64)
+        #error cmake_ARCH arm64
+#elif defined(__arm__) \\
+   || defined(__TARGET_ARCH_ARM) \\
+   || defined(_M_ARM)
+        #error cmake_ARCH arm
+#elif defined(__i386) \\
+   || defined(__i386__) \\
+   || defined(_M_IX86)
+        #error cmake_ARCH x86
+#elif defined(__x86_64) \\
+   || defined(__x86_64__) \\
+   || defined(__amd64) \\
+   || defined(_M_X64)
+        #error cmake_ARCH x86_64
+#elif defined(__ia64) \\
+   || defined(__ia64__) \\
+   || defined(_M_IA64)
+        #error cmake_ARCH ia64
 #elif defined(__EMSCRIPTEN__)
-	#error cmake_ARCH any
+        #error cmake_ARCH any
 #endif
 
 #error cmake_ARCH unknown
-")
+"
+)
 
 function(target_architecture output_var)
-	file(WRITE "${TINYSPLINE_BINARY_DIR}/arch.c" "${archdetect_c_code}")
+  file(WRITE "${TINYSPLINE_BINARY_DIR}/arch.c" "${archdetect_c_code}")
 
-	enable_language(C)
+  enable_language(C)
 
-	# Detect the architecture in a rather creative way...
-	# This compiles a small C program which is a series of ifdefs that
-	# selects a particular #error preprocessor directive whose message
-	# string contains the target architecture. The program will always fail
-	# to compile (both because file is not a valid C program, and obviously
-	# because of the presence of the #error preprocessor directives... but
-	# by exploiting the preprocessor in this way, we can detect the correct
-	# target architecture even when cross-compiling, since the program
-	# itself never needs to be run (only the compiler/preprocessor)
-	try_run(
-		run_result_unused
-		compile_result_unused
-		"${TINYSPLINE_BINARY_DIR}"
-		"${TINYSPLINE_BINARY_DIR}/arch.c"
-		COMPILE_OUTPUT_VARIABLE ARCH
-	)
+  # Detect the architecture in a rather creative way...
+  #
+  # This compiles a small C program which is a series of ifdefs that selects a
+  # particular #error preprocessor directive whose message string contains the
+  # target architecture. The program will always fail to compile (both because
+  # the file is not a valid C program, and obviously because of the presence of
+  # the #error preprocessor directives). By exploiting the preprocessor in this
+  # way, we can detect the correct target architecture even when
+  # cross-compiling, since the program itself never needs to be run (only the
+  # compiler/preprocessor).
+  try_run(run_result_unused compile_result_unused "${TINYSPLINE_BINARY_DIR}"
+          "${TINYSPLINE_BINARY_DIR}/arch.c" COMPILE_OUTPUT_VARIABLE ARCH
+  )
 
-	# Parse the architecture name from the compiler output
-	string(REGEX MATCH "cmake_ARCH ([a-zA-Z0-9_]+)" ARCH "${ARCH}")
+  # Parse the architecture name from the compiler output.
+  string(REGEX MATCH "cmake_ARCH ([a-zA-Z0-9_]+)" ARCH "${ARCH}")
 
-	# Get rid of the value marker leaving just the architecture name
-	string(REPLACE "cmake_ARCH " "" ARCH "${ARCH}")
+  # Get rid of the value marker, leaving just the architecture name.
+  string(REPLACE "cmake_ARCH " "" ARCH "${ARCH}")
 
-	# If we are compiling with an unknown architecture this variable should
-	# already be set to "unknown" but in the case that it's empty (i.e. due
-	# to a typo in the code), then set it to unknown
-	if (NOT ARCH)
-		set(ARCH unknown)
-	endif()
+  # If we are compiling for an unknown architecture, the following variable
+  # should already be set to "unknown". If the variable is empty (e.g., due to a
+  # typo in the code), then explicitly set it to "unknown".
+  if(NOT ARCH)
+    set(ARCH unknown)
+  endif()
 
-	set(${output_var} "${ARCH}" PARENT_SCOPE)
+  set(${output_var}
+      "${ARCH}"
+      PARENT_SCOPE
+  )
 endfunction()
